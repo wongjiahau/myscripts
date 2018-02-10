d21738ec5d555d48c1faba81084452ceb5ccb997
Wong Jia Hau <hou32hou@gmail.com>
2018-02-10 08:38:01 +0800

    Update

--- myconfigs/config.fish
+++ myconfigs/config.fish
@@ -8,3 +8,9 @@ source $OMF_PATH/init.fish
 set -gx PATH ~/myscripts $PATH
 fish_vi_mode
 
+# aliases
+alias sizeofhere="du -hs ./"
+alias duh="du -h"
+alias del="gvfs-trash" 
+alias rm="echo Use 'del' instead, because rm is dangerous, del will delete the item to trash"
+alias py="python"
