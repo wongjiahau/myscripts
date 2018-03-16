a68d9805a9c5c428ac1170590af9083942be9630
Wong Jia Hau <hou32hou@gmail.com>
2018-03-16 08:56:01 +0800

    Update

--- myconfigs/init.sh
+++ myconfigs/init.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+
+# Link config.fish
 rm ~/.config/fish/config.fish
+DIR=$(pwd)
+ln -s "$DIR/config.fish" ~/.config/fish/
+source ~/.config/fish/config.fish
+
+# Link .gitconfig
+ln -s "$DIR/.gitconfig" ~/
+
